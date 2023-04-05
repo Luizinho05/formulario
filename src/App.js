05190bb8dc3b6193158fc09a90df8994bb5bd9b5
Luiz Henrique de Oliveira <luizhsimionato@gmail.com>
2023-04-05 16:19:12 -0300

    formulario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+constructor(props){
+  super(props)
+  this.state  = {
+    nome: '',
+    email:'',
+    senha:'',
+  }
+  this.cadastrar = this.cadastrar.bind(this)
+}
+
+cadastrar(event){
+  event.preventDefault()
+  const {nome, email, senha} = this.state
+  alert(`Nome: ${nome} \nEmail: ${email} \nSenha: ${senha}`)
+  
+}
+
+  render(){
+      return(
+
+        <div>
+          <h1>Cadastro de Usuário</h1>
+
+          <form onSubmit={this.cadastrar}>
+
+            <label>Nome:</label>
+            <input
+            type='text'
+            value={this.state.nome}
+            onChange={ (e) => this.setState({ nome: e.target.value })}
+             /><br />
+            <label>email:</label>
+            <input 
+            type='text'
+            value={this.state.email}
+            onChange={ (e) => this.setState({ email: e.target.value })}
+            /><br />
+            <label>Senha:</label>
+            <input 
+            type='text'
+            value={this.state.senha}
+            onChange={ (e) => this.setState({ senha: e.target.value })}
+            /><br />
+            <button type='submit'>Cadastrar</button>
+
+          </form>
+        </div>
+      )
+     }   
 }
 
 export default App;
